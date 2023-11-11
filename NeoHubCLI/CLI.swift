355dcc27096fdc7cdc54a06efbd1a906d13776f9
Alex Fedoseev <alex@fedoseev.mx>
2023-11-11 12:54:15 +0400

    v0.1.1

--- NeoHubCLI/CLI.swift
+++ NeoHubCLI/CLI.swift
@@ -33,7 +33,7 @@ struct CLI: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "neohub",
         abstract: "A CLI interface to NeoHub. Launch new or activate already running Neovide instance.",
-        version: "0.0.1"
+        version: "0.1.1"
     )
 
     @Argument(help: "Optional path passed to Neovide.")
